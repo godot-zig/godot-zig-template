1bee69516f0b118541858cb3b28633c767a0eac4
Elvis Zhou <elvis.x.zhou@gmail.com>
2024-04-29 16:12:55 +0800

    update to latest godot-zig

--- build.zig
+++ build.zig
@@ -14,6 +14,11 @@ pub fn build(b: *std.Build) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
+    const precision = b.option([]const u8, "precision", "double") orelse "float";
+    const arch = b.option([]const u8, "arch", "32") orelse "64";
+    const godot_path = b.option([]const u8, "godot", "godot path") orelse "godot";
+    const godot_zig_build = @import("godot-zig/build.zig");
+    _ = godot_zig_build.createBindStep(b, target, precision, arch, godot_path);
 
     const lib = b.addSharedLibrary(.{
         .name = "Template",
@@ -24,11 +29,14 @@ pub fn build(b: *std.Build) void {
 
     const api_path = "./godot-zig/src/api";
 
-    const godot_zig_build = @import("godot-zig/build.zig");
-    _ = godot_zig_build.createBindStep(b, target);
-
     const mod = b.createModule(.{ .root_source_file = .{ .path = api_path ++ "/Godot.zig" } });
     mod.addIncludePath(.{ .path = api_path });
+
+    const build_options = b.addOptions();
+    build_options.addOption([]const u8, "precision", precision);
+    build_options.addOption([]const u8, "arch", arch);
+    mod.addOptions("build_options", build_options);
+
     lib.root_module.addImport("Godot", mod);
 
     lib.addIncludePath(.{ .path = api_path });
@@ -37,7 +45,11 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(lib);
 
     const run_cmd = b.addSystemCommand(&.{
-        "godot", "--path", "./project",
+        "godot",
+        "--path",
+        "./project",
+        "--resolution",
+        "1920x1080",
     });
     run_cmd.step.dependOn(b.getInstallStep());
     const run_step = b.step("run", "run with Godot");
